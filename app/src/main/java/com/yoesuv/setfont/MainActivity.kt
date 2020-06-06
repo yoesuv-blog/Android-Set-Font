a3aa4d4402d087ef3b4e89a9b889ae0000187a8b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-06-06 11:47:02 +0700

    :construction: build boilerplate

--- app/src/main/java/com/yoesuv/setfont/MainActivity.kt
+++ app/src/main/java/com/yoesuv/setfont/MainActivity.kt
@@ -2,10 +2,20 @@ package com.yoesuv.setfont
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbar)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
     }
 }
